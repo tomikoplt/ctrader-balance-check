ef0d95f6f0150a142f8ef34b8a4ea6cbeca465c0
tomikoplt <98596981+tomikoplt@users.noreply.github.com>
2026-04-28 13:35:09 +0200

    Update script.py

--- script.py
+++ script.py
@@ -4,18 +4,22 @@ import requests
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-# TEMP: simulate balance (we'll replace this later)
-balance = 1600   # change this to test
+balance = 1600
 
 if balance > 1500:
     message = f"Balance alert: {balance} EUR"
 else:
     message = f"Balance OK: {balance} EUR"
 
-requests.post(
+response = requests.post(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
     data={
         "chat_id": CHAT_ID,
         "text": message,
     },
 )
+
+print("STATUS:", response.status_code)
+print("RESPONSE:", response.text)
+
+response.raise_for_status()

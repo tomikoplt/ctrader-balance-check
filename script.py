e5c3e9fc06533dee715d7a84aaa71c99fc9a00f3
tomikoplt <98596981+tomikoplt@users.noreply.github.com>
2026-04-28 13:15:34 +0200

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,18 @@
-import requests
 import os
+import requests
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
 message = "Test message from GitHub Actions"
 
-requests.post(
+response = requests.post(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
-    data={"chat_id": CHAT_ID, "text": message}
+    data={
+        "chat_id": CHAT_ID,
+        "text": message,
+    },
 )
+
+print(response.status_code)
+print(response.text)
